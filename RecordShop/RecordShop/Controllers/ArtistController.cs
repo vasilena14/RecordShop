99abcd66c465e1e7dea0502256172c7f2b7ffb73
vasilena14 <wasito98@gmail.com>
2021-01-30 21:44:48 +0200

    Created Index and Create View for Artists

--- RecordShop/RecordShop/Controllers/ArtistController.cs
+++ RecordShop/RecordShop/Controllers/ArtistController.cs
@@ -3,14 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using RecordShop.AppDbContext;
+using RecordShop.Models;
 
 namespace RecordShop.Controllers
 {
     public class ArtistController : Controller
     {
+        private readonly RecordShopDbContext _db;
+        public ArtistController(RecordShopDbContext db)
+        {
+            _db = db;
+        }
+
+
         public IActionResult Index()
         {
+            return View(_db.Artists.ToList());
+        }
+
+        //HTTP Get Method
+        public IActionResult Create()
+        {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Artist artist)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Add(artist);
+                _db.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(artist);
         }
     }
 }
