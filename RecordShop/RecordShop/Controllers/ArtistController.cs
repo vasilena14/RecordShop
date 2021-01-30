0c56a09d8fd944a6efb712288e509eeef54cbe87
vasilena14 <wasito98@gmail.com>
2021-01-30 22:38:00 +0200

    Created Delete method and Edit View for Artists

--- RecordShop/RecordShop/Controllers/ArtistController.cs
+++ RecordShop/RecordShop/Controllers/ArtistController.cs
@@ -39,5 +39,42 @@ namespace RecordShop.Controllers
             }
             return View(artist);
         }
+
+        //Post Method
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            Artist artist = _db.Artists.Find(id);
+            if (artist == null)
+            {
+                return NotFound();
+            }
+            _db.Artists.Remove(artist);
+            _db.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var artist = _db.Artists.Find(id);
+            if (artist == null)
+            {
+                return NotFound();
+            }
+            return View(artist);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Artist artist)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Artists.Update(artist);
+                _db.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(artist);
+        }
     }
 }
