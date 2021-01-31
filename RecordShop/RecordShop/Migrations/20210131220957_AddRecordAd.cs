f8b5955fd0beb6341d47cac4b49904fdba7ce514
vasilena14 <wasito98@gmail.com>
2021-02-01 00:12:17 +0200

    Created RecordAd model and added migration

---
+++ RecordShop/RecordShop/Migrations/20210131220957_AddRecordAd.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RecordShop.Migrations
+{
+    public partial class AddRecordAd : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "AspNetUserTokens",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)",
+                oldMaxLength: 128);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LoginProvider",
+                table: "AspNetUserTokens",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)",
+                oldMaxLength: 128);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ProviderKey",
+                table: "AspNetUserLogins",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)",
+                oldMaxLength: 128);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LoginProvider",
+                table: "AspNetUserLogins",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)",
+                oldMaxLength: 128);
+
+            migrationBuilder.CreateTable(
+                name: "RecordAds",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ArtistID = table.Column<int>(nullable: false),
+                    AlbumID = table.Column<int>(nullable: false),
+                    Country = table.Column<string>(nullable: false),
+                    Format = table.Column<string>(nullable: false),
+                    Genre = table.Column<string>(nullable: false),
+                    SellerName = table.Column<string>(nullable: false),
+                    SellerEmail = table.Column<string>(nullable: true),
+                    SellerPhone = table.Column<string>(nullable: false),
+                    Price = table.Column<int>(nullable: false),
+                    Currency = table.Column<string>(nullable: false),
+                    ImagePath = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RecordAds", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_RecordAds_Albums_AlbumID",
+                        column: x => x.AlbumID,
+                        principalTable: "Albums",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.NoAction);
+                    table.ForeignKey(
+                        name: "FK_RecordAds_Artists_ArtistID",
+                        column: x => x.ArtistID,
+                        principalTable: "Artists",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.NoAction);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RecordAds_AlbumID",
+                table: "RecordAds",
+                column: "AlbumID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RecordAds_ArtistID",
+                table: "RecordAds",
+                column: "ArtistID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RecordAds");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "AspNetUserTokens",
+                type: "nvarchar(128)",
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LoginProvider",
+                table: "AspNetUserTokens",
+                type: "nvarchar(128)",
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ProviderKey",
+                table: "AspNetUserLogins",
+                type: "nvarchar(128)",
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LoginProvider",
+                table: "AspNetUserLogins",
+                type: "nvarchar(128)",
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(string));
+        }
+    }
+}
---
+++ RecordShop/RecordShop/Models/RecordAd.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RecordShop.Models
+{
+    public class RecordAd
+    {
+        public int Id { get; set; }
+        public Artist Artist { get; set; }
+        public int ArtistID { get; set; }
+        public Album Album { get; set; }
+        public int AlbumID { get; set; }
+        [Required]
+        [DisplayName("Release Country")]
+        public string Country { get; set; }
+        [Required]
+        public string Format { get; set; }
+        [Required]
+        public string Genre { get; set; }
+        [Required]
+        [DisplayName("Seller Name")]
+        public string SellerName { get; set; }
+        [DisplayName("Seller Email")]
+        public string SellerEmail { get; set; }
+        [Required]
+        [DisplayName("Seller Phone Number")]
+        public string SellerPhone { get; set; }
+        [Required] 
+        public int Price { get; set; }
+        [Required] 
+        public string Currency { get; set; }
+        public string ImagePath { get; set; }
+    }
+}
